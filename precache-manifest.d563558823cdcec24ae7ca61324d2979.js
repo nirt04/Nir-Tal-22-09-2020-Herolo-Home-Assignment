762327dd9e98c1b73f8059e6c3cbe89b6237be88
Nir <nirt04@gmail.com>
2020-09-27 02:48:27 +0300

    Updates

---
+++ precache-manifest.d563558823cdcec24ae7ca61324d2979.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c94bf4aad9cd09eb7a49c3dff55b179",
+    "url": "/Nir-Tal-22-09-2020-Herolo-Home-Assignment/index.html"
+  },
+  {
+    "revision": "ad39b0cee9038e5f4bdd",
+    "url": "/Nir-Tal-22-09-2020-Herolo-Home-Assignment/static/css/main.85e1fc43.chunk.css"
+  },
+  {
+    "revision": "d09e8c78a8eced0398b1",
+    "url": "/Nir-Tal-22-09-2020-Herolo-Home-Assignment/static/js/2.508c3d7a.chunk.js"
+  },
+  {
+    "revision": "177dce6ef9045384a932287ff9d95cbd",
+    "url": "/Nir-Tal-22-09-2020-Herolo-Home-Assignment/static/js/2.508c3d7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad39b0cee9038e5f4bdd",
+    "url": "/Nir-Tal-22-09-2020-Herolo-Home-Assignment/static/js/main.b39235e9.chunk.js"
+  },
+  {
+    "revision": "b3cc8ebc4637d29bb881",
+    "url": "/Nir-Tal-22-09-2020-Herolo-Home-Assignment/static/js/runtime-main.19da98f5.js"
+  }
+]);
