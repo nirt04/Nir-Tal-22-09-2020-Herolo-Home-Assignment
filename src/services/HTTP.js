0a9b50fdf2a8a70a76b68a7f43fe886ca8a97139
Nir <nirt04@gmail.com>
2020-09-29 10:37:25 +0300

    key

--- src/services/HTTP.js
+++ src/services/HTTP.js
@@ -11,7 +11,7 @@ const HTTP = axios.create({
     'Content-Type': 'application/json',
   },
   params: {
-    apikey: 'nHz7ILclGvWOyqQQA7y3kASkAan1oQ34' || 'A93ZHWxuSpGJ3A4OlGiTs7dWWgLLDRKD' || 'xFGvCy1FaaIkGtmG3Y3fLsuTE1lpdUVX' || 'JpXZNXKBXpKo5VVG0RZzybpKahdMX8vv' || 'xFGvCy1FaaIkGtmG3Y3fLsuTE1lpdUVX' || 'rahPnD6qt8hT5e5c1tyfpo5SG51pefEB' || 'nHz7ILclGvWOyqQQA7y3kASkAan1oQ34' || 'yorJNfSt5GWC6ym3UEo8WZGX0N5eyRhb',
+    apikey: 'CNaQZfKNEpgWU5zO2MM3GAUv6QkoeeGh' || 'nHz7ILclGvWOyqQQA7y3kASkAan1oQ34' || 'A93ZHWxuSpGJ3A4OlGiTs7dWWgLLDRKD' || 'xFGvCy1FaaIkGtmG3Y3fLsuTE1lpdUVX' || 'JpXZNXKBXpKo5VVG0RZzybpKahdMX8vv' || 'xFGvCy1FaaIkGtmG3Y3fLsuTE1lpdUVX' || 'rahPnD6qt8hT5e5c1tyfpo5SG51pefEB' || 'nHz7ILclGvWOyqQQA7y3kASkAan1oQ34' || 'yorJNfSt5GWC6ym3UEo8WZGX0N5eyRhb',
   },
 });
 
