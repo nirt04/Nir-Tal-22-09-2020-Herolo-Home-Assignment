bda043d583115f84e0258d0348c2f9a2ac1f8f99
Nir <nirt04@gmail.com>
2020-09-29 08:31:40 +0300

    eslinter

--- src/plugins/material-ui.js
+++ src/plugins/material-ui.js
@@ -4,7 +4,7 @@ export const theme = (props) => createMuiTheme({
   palette: {
     type: props.appConfig.themeType,
     background: {
-      paper: props.appConfig.themeType == 'dark' ? '#3810ae' : '#FFFF',
+      paper: props.appConfig.themeType === 'dark' ? '#3810ae' : '#FFFF',
     },
     innerCard: {
       light: '#FFFF',
